64ebd3c0b0f4c9974abb12dac76288ffa9b13970
Natalia <Nataly94ku@gmail.com>
2020-06-08 08:10:47 -0500

    my fist git!!

---
+++ Sunny School/Pages/DatosFamiliares.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sunny_School.Pages
+{
+    public class DatosFamiliaresModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
